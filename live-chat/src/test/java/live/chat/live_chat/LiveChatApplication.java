9ed4bbb48a2534fd10351abf39b6ab1dbb862541
mikeymaikh <94535202+mikeymaikh@users.noreply.github.com>
2025-07-31 10:00:16 +0000

    Add initial Spring Boot application structure with Maven configuration

---
+++ live-chat/src/test/java/live/chat/live_chat/LiveChatApplication.java
@@ -0,0 +1,15 @@
+package live.chat.live_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
+
