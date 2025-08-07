13e460a5515befad07fe1fdc20170fb0dff1f3f1
Mikael Gourine <mikael.gourine@edu.karelia.fi>
2025-08-07 15:16:03 +0300

    Fix import statement in MessageRepository.java

--- live-chat/src/main/java/live/chat/live_chat/model/MessageRepository.java
+++ live-chat/src/main/java/live/chat/live_chat/model/MessageRepository.java
@@ -1,7 +1,7 @@
 package live.chat.live_chat.model;
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import  live.chat.live_chat.model.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findBySenderId(Long senderId);
